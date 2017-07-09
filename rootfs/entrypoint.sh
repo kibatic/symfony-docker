72accfb27027042906dcdb71f31b5f381e2be760
Elie CHARRA <e1ie@protonmail.com>
2017-07-09 23:32:08 +0200

    Fix entrypoint script

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+BASE_DIR=/var/www/
 # Following folders must be writables by www-data web user
+# Handle Symfony 3.x directory layout (var/...) and legacy layout (app/...)
 RW_FOLDERS=(var/cache var/logs var/sessions app/cache app/logs)
 
-for dir in $RW_FOLDERS; do
-  if [ -d "/var/www/$dir" ]; then
-    echo "Fix permission for /var/www/$dir"
-    chown -R www-data /var/www/$dir
+for dir in "${RW_FOLDERS[@]}"; do
+  if [ -d "$BASE_DIR$dir" ]; then
+    echo "Fix permission for $BASE_DIR$dir"
+    chown -R www-data $BASE_DIR$dir
   fi
 done
 
