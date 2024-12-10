308e3d2300d76eb596f587b5ee139de332d6a3ba
Philippe Le Van <philippe.levan@kibatic.com>
2024-12-10 18:08:11 +0100

    new 7.4 version

---
+++ 7.4-fpm-bullseye/rootfs/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+BASE_DIR=/var/www/
+# Following folders must be writables by www-data web user
+RW_FOLDERS=(var)
+
+for dir in "${RW_FOLDERS[@]}"; do
+  if [ -d "$BASE_DIR$dir" ]; then
+    echo "Fix permission for $BASE_DIR$dir"
+    chown -R www-data $BASE_DIR$dir
+  fi
+done
+
+# Disable opcache optimisation for developpement
+# Allow files to be reloaded when update without restarting fpm process
+if [[ "$PERFORMANCE_OPTIM" = "false" ]]
+then
+  echo "Disable performance optimisation"
+  echo > /usr/local/etc/php/conf.d/99-symfony.ini
+fi
+
+exec /usr/bin/supervisord -n
