7604ace7a1686c9c27dbd56451112455cd29e193
Philippe Le Van <philippe.levan@kibatic.com>
2025-05-26 15:14:45 +0200

    image 7.4 fpm avec choix de la distrib symfony

--- 7.4-fpm-bullseye/rootfs/entrypoint.sh
+++ 7.4-fpm-bullseye/rootfs/entrypoint.sh
@@ -11,6 +11,17 @@ for dir in "${RW_FOLDERS[@]}"; do
   fi
 done
 
+SYMFONY_VERSION=${SYMFONY_VERSION:-5}
+NGINX_CONFIG=symfony$SYMFONY_VERSION
+
+# Avoid to remove a bind mounted nginx config
+NGINX_DEFAULT=/etc/nginx/sites-enabled/default
+if ! mountpoint -q $NGINX_DEFAULT; then
+  echo "Using default nginx config : $NGINX_CONFIG"
+  rm $NGINX_DEFAULT
+  ln -s /etc/nginx/sites-available/$NGINX_CONFIG.conf $NGINX_DEFAULT
+fi
+
 # Disable opcache optimisation for developpement
 # Allow files to be reloaded when update without restarting fpm process
 if [[ "$PERFORMANCE_OPTIM" = "false" ]]
